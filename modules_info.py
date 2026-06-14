2f2823a3bf6c38d6c81b8fb2c5726be4319ac273
provokaciya-osint <stray-twister-open@duck.com>
2026-06-14 19:10:05 +0500

    add modules info script

---
+++ modules_info.py
@@ -0,0 +1,13 @@
+from modules import loader
+
+loader.load_all()
+
+for name, entry in loader.all_modules().items():
+	meta = entry["meta"]
+	print(
+            f"Модуль: {meta["name"]}",
+            f"Версия: {meta["version"]}",
+            f"Описание: {meta["description"]}",
+            f"Автор: {meta["author"]}",
+            sep="\n"
+    )
