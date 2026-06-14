154ec03837700f0c352da7df711f7f87229cfc2c
provokaciya-osint <stray-twister-open@duck.com>
2026-06-14 18:58:24 +0500

    remove useless file

--- modules/validate/validate.py
+++
@@ -1,7 +0,0 @@
-from phonenumbers import is_valid_number as is_valid
-from phonenumbers import parse
-
-def validate(number: str):
-    parsed_number = parse(number)
-    result = is_valid(parsed_number)
-    return result
