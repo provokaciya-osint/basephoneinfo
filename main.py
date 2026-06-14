60666b181c7a6bd01f46cd658d1abf5fca7d9f5c
provokaciya-osint <stray-twister-open@duck.com>
2026-06-14 18:46:07 +0500

    make validate as a separate module

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import phonenumbers as phone
 from os import getenv
-import modules.actions as actions
+from modules.actions.validate import validate
+
+number = getenv("number")
 
 ACTIONS_LIST = {
-    "Валидация": {
+    "Validate": {
+        "name": "Валидация номера",
         "description": "Проверить валидность номера",
-        "action": actions.validate.validate
+        "action": validate
     }
 }
 
-def menu():
+def get_names():
+    names = []
+    for data in ACTIONS_LIST.values():
+        names.append(data["name"])
+    return names
+
+def print_menu():
+    for name in get_names():
+        print(name)
+
+def select_action():
     action = input(">>> ")
-    print(type(ACTIONS_LIST[action]["action"]))
+
+print_menu()
+select_action()
--- modules/actions/validate.py
+++
@@ -1,5 +0,0 @@
-from phonenumbers import is_valid_number as is_valid
-
-def validate(number: str):
-    result = is_valid(number)
-    return result
